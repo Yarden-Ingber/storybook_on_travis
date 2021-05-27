2ff2369fbf9d98ea4d52117f5d042b4d7e7b5299
Yarden Ingber <54064203+Yarden-Ingber@users.noreply.github.com>
2021-05-27 03:13:50 +0300

    Update index.js

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   name: 'eyes_selenium_java',
+  name2: 'eyes_selenium_java2',
   emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/emitter.js',
   overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/overrides.js',
   template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/template.hbs',
